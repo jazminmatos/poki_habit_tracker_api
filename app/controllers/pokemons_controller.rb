9c5161924c4d1044f1f98ba9dd901e9094d6da37
jazminmatos <jmatos1993@gmail.com>
2021-09-22 17:44:15 -0400

    changed instance variables to local variables

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -3,45 +3,45 @@ class PokemonsController < ApplicationController
 
   # GET /pokemons
   def index
-    @pokemons = Pokemon.all
+    pokemons = Pokemon.all
 
-    render json: @pokemons
+    render json: pokemons
   end
 
   # GET /pokemons/1
   def show
-    render json: @pokemon
+    render json: pokemon
   end
 
   # POST /pokemons
   def create
-    @pokemon = Pokemon.new(pokemon_params)
+    pokemon = Pokemon.new(pokemon_params)
 
-    if @pokemon.save
-      render json: @pokemon, status: :created, location: @pokemon
+    if pokemon.save
+      render json: pokemon, status: :created, location: pokemon
     else
-      render json: @pokemon.errors, status: :unprocessable_entity
+      render json: pokemon.errors, status: :unprocessable_entity
     end
   end
 
   # PATCH/PUT /pokemons/1
   def update
-    if @pokemon.update(pokemon_params)
-      render json: @pokemon
+    if pokemon.update(pokemon_params)
+      render json: pokemon
     else
-      render json: @pokemon.errors, status: :unprocessable_entity
+      render json: pokemon.errors, status: :unprocessable_entity
     end
   end
 
   # DELETE /pokemons/1
   def destroy
-    @pokemon.destroy
+    pokemon.destroy
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_pokemon
-      @pokemon = Pokemon.find(params[:id])
+      pokemon = Pokemon.find(params[:id])
     end
 
     # Only allow a list of trusted parameters through.
